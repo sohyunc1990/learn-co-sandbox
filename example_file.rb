b50f4eb460c447ab5a326f09e73bc3cd731a68b6
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-13 13:34:50 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,10 @@
+puts "Hello World\n"
+Hello World
+
+spice_rack = [
+  ["Mace", "Ginger", "Marojam"],          
+  ["Paprika", "Fajita Mix", "Coriander"], 
+  ["Parsley", "Sage", "Rosemary"]         
+]
+
+spice_rack[1][1]
